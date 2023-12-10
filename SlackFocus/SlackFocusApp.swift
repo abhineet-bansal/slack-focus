0f648e514bf204bca013edb0b07520c65bceedb4
Abhineet Bansal <abhineet@canva.com>
2023-12-10 12:24:41 +1100

    Initial Commit

---
+++ SlackFocus/SlackFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlackFocusApp.swift
+//  SlackFocus
+//
+//  Created by Abhineet Bansal on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SlackFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
